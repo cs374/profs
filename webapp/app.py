426846651d9f53d5569f0248c48ef7da6cbf73dc
Chris Mayfield <mayfiecs@jmu.edu>
2024-10-28 23:43:57 -0400

    added example table chart and workshop form

---
+++ webapp/app.py
@@ -0,0 +1,45 @@
+"""Web app for madiSTEM Workshop Management."""
+
+from flask import Flask, render_template, request, flash, redirect
+import db
+
+app = Flask(__name__)
+app.secret_key = "dev"  # required for flash()
+
+
+@app.route("/")
+def index():
+    return render_template("index.jinja")
+
+
+@app.route("/workshops")
+def workshop_all():
+    data = db.workshop_all()
+    return render_template("workshop_all.jinja", data=data)
+
+
+@app.route("/workshops/<key>")
+def workshop_edit(key):
+    if request.args:
+        action = request.args.get("action")
+        try:
+            if action == "Insert":
+                values = list(request.args.values())
+                del values[13]  # action
+                db.workshop_ins(values)
+                flash("Workshop inserted")
+            elif action == "Update":
+                flash("Update not implemented")
+            elif action == "Delete":
+                key = request.args["ID"]
+                db.workshop_del(key)
+                flash("Workshop deleted")
+        except Exception as e:
+            flash(str(e))
+        return redirect("/workshops")
+    # Form was not submitted
+    if key == "new":
+        data = []
+    else:
+        data = db.workshop_get(key)
+    return render_template("workshop_edit.jinja", data=data)
