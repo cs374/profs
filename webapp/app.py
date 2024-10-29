66b4acbfb1e123dde7ac14f2a600b0789dd758f5
Mona Rizvi <maerizvi@gmail.com>
2024-10-29 11:41:19 -0400

    switched the order of the checks in the workshop_edit route

--- webapp/app.py
+++ webapp/app.py
@@ -20,28 +20,28 @@ def workshop_all():
 
 @app.route("/workshops/<key>")
 def workshop_edit(key):
+    # If the form was not submitted (i.e., first visit)
+    if not request.args:
+        if key == "new":
+            values = []
+        else:
+            values = db.workshop_get(key)
+        return render_template("workshop_edit.jinja", key=key, values=values)
 
-    if request.args:
-        values = list(request.args.values())
-        action = values.pop()
-        try:
-            if action == "Insert":
-                db.workshop_ins(values)
-                flash("Workshop inserted")
-            elif action == "Update":
-                flash("Update not implemented")
-            elif action == "Delete":
-                key = request.args["ID"]
-                db.workshop_del(key)
-                flash("Workshop deleted")
-        except Exception as e:
-            flash(str(e))
-            return render_template("workshop_edit.jinja", key=key, values=values)
-        return redirect("/workshops")
-
-    # Form was not submitted
-    if key == "new":
-        values = []
-    else:
-        values = db.workshop_get(key)
-    return render_template("workshop_edit.jinja", key=key, values=values)
+    # Perform the requested action
+    values = list(request.args.values())
+    action = values.pop()
+    try:
+        if action == "Insert":
+            db.workshop_ins(values)
+            flash("Workshop inserted")
+        elif action == "Update":
+            flash("Update not implemented")
+        elif action == "Delete":
+            key = request.args["ID"]
+            db.workshop_del(key)
+            flash("Workshop deleted")
+    except Exception as e:
+        flash(str(e))
+        return render_template("workshop_edit.jinja", key=key, values=values)
+    return redirect("/workshops")
