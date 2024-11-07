051b8b4cf0ffdc5acf8f252ee039d90eca9a3d08
Chris Mayfield <mayfiecs@jmu.edu>
2024-11-07 10:36:24 -0500

    added favicon

--- webapp/app.py
+++ webapp/app.py
@@ -1,6 +1,7 @@
 """Web app for madiSTEM Workshop Management."""
 
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 app.secret_key = "dev"  # required for flash()
@@ -9,6 +10,13 @@ app.secret_key = "dev"  # required for flash()
 @app.route("/")
 def index():
     return render_template("index.jinja")
+
+
+@app.route("/favicon.ico")
+def favicon():
+    # See https://flask.palletsprojects.com/en/stable/patterns/favicon/
+    return send_from_directory(os.path.join(app.root_path, "static"),
+                               "favicon.ico", mimetype="image/vnd.microsoft.icon")
 
 
 # Import all routes from other modules
