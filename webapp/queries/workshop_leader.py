e5e165c7b936257da202b84c702e923693b6abbc
Mona Rizvi <maerizvi@gmail.com>
2024-11-14 22:52:14 -0500

    Updated GP5 code (check the port number)

--- webapp/queries/workshop_leader.py
+++ webapp/queries/workshop_leader.py
@@ -18,7 +18,35 @@ def workshop_get(email, year):
                    AND w.event_year = %s""", [email,year])
             return cur.fetchone()
 
-
+def workshop_update(email, year, values):
+    with db_connect() as con:
+        with con.cursor() as cur:
+            cur.execute(
+                """SELECT w.id
+                   FROM workshop w
+                   join person_workshop pw
+                   ON w.id = pw.workshop_id
+                   WHERE pw.person_email = %s
+                   AND pw.role = "Lead"
+                   AND w.event_year = %s""", [email,year])
+            id =  cur.fetchone()[0]
+            values.append(id)
+            print("ID", id)
+            cur.execute(
+                """UPDATE workshop
+                    SET title = %s,
+                    advertisement = %s,
+                    description = %s,
+                    capacity = %s,
+                    computer_needs = %s,
+                    room_needs = %s,
+                    max_repeat = %s,
+                    parent_questions = %s,
+                    other_information = %s
+                   WHERE id = %s
+                   """, values)
+            con.commit()
+            return id
 
 
 if __name__ == "__main__":
--- webapp/routes/workshop_leader.py
+++ webapp/routes/workshop_leader.py
@@ -7,12 +7,45 @@ from flask import render_template, request, flash, redirect
 
 @app.route("/workshop_leader")
 def workshop_leader():
+    # If the form was not submitted
+    if not request.args:
+        return render_template("functionality/workshop_leader.jinja")
     email = request.args.get("email")
     year = request.args.get("year")
     if email and year:
+        return redirect("/workshop_leader/" + email + "/" + year)
+    flash("Must enter both values.")
+    return render_template("functionality/workshop_leader.jinja")
+
+
+
+@app.route("/workshop_leader/<email>/<year>")
+def workshop_leader_edit(email, year):
+    # If the form was not submitted
+    print(email, year, "HEY")
+    if not request.args:
         data = db.workshop_get(email, year)
-    else:
-        data = None
-        flash("No workshop found for this person.")
-    return render_template("views/workshop_leader.jinja",
+        if not data:
+            flash("No workshop found for this person.")
+            return redirect("/workshop_leader")
+        data = ["" if v is None else v for v in data]
+        return render_template("functionality/wl_edit.jinja",
                            email=email, year=year, data=data)
+
+    # Update
+    values = list(request.args.values())
+    action = values.pop()
+    try:
+        if action == "Update":
+            # integer values
+            if values[3].strip() == "":
+                values[3] = 0
+            if values[6].strip() == "":
+                values[6] = 0
+            id = db.workshop_update(email, year, values)
+            flash("Updated workshop #" + str(id))
+    except Exception as e:
+        flash(str(e))
+        return render_template("functionality/wl_edit.jinja",
+                           email=email, year=year, data=values)
+    return redirect("/workshop_leader")
